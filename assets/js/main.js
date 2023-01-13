21fdf909a2727fb61381949ed00ccba2666630e6
Bogdan Bogdanovic <bogdi.mail@gmail.com>
2023-01-13 20:20:04 +0100

    added live-server package and typescript package

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
+"use strict";
 (function () {
-  // Add copyright to the footer
-  const addCopyrightYearFooter = () => {
-    document.getElementById(
-      "copyright"
-    ).innerHTML = `&copy; ${new Date().getFullYear()} Magdalena Barna`;
-  };
-  addCopyrightYearFooter();
+    // Add copyright to the footer
+    const addCopyrightYearFooter = () => {
+        document.getElementById("copyright").innerHTML = `&copy; ${new Date().getFullYear()} Magdalena Barna`;
+    };
+    addCopyrightYearFooter();
 })();
---
+++ assets/js/main.ts
@@ -0,0 +1,10 @@
+"use strict";
+(function () {
+  // Add copyright to the footer
+  const addCopyrightYearFooter = () => {
+    document.getElementById(
+      "copyright"
+    )!.innerHTML = `&copy; ${new Date().getFullYear()} Magdalena Barna`;
+  };
+  addCopyrightYearFooter();
+})();
